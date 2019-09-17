199596867e7bf80fdbbdc3d9f8548112a65cd844
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-09-17 22:05:40 +0300

    add constructor

--- src/com/uits/task9/test.java
+++ src/com/uits/task9/test.java
@@ -1,4 +1,24 @@
 package com.uits.task9;
 
 public class test {
+    int Age;
+    String Name;
+
+    test(int age, String name) {
+        System.out.println("Конструирование объекта с двумя аргументами");
+        Age = age;
+        Name = name;
+    }
+
+    test(String name) {
+        System.out.println("Конструирование объекта с одним аргументом name");
+        Name = name;
+        Age=0;
+    }
+    test() {
+        System.out.println("Конструирование объекта без аргументов");
+        Name = null;
+        Age = 0;
+    }
+
 }
