6f67c0f93afd03d97aa8c558df35a5d4b262eba4
SergMicar <Sergey.V.Goncharov@gmail.com>
2019-09-25 13:07:55 +0300

    add call printMatrix

--- src/com/uits/task9/MainMatrix.java
+++ src/com/uits/task9/MainMatrix.java
@@ -13,18 +13,25 @@ package com.uits.task9;
 public class MainMatrix {
     public static void main(String[] args) {
         // Тестирование первого конструктора
+        System.out.println("Создаем обьект matrix1");
         Matrix matrix1 = new Matrix(4,5); // создание объекта
+        System.out.println("Создаем обьект matrix2");
         Matrix matrix2 = new Matrix(6,3);
 
         // Тестирование второго конструктора
-        int p = (int) (Math.random() * 7 + 2);
-        int q = (int) (Math.random() * 7 + 2);
-        double[][] ar = new double[p][q];
-        System.out.println("Матрица2: ");
-        test.fillMatrix(ar);                       // заполнение матрицы
-        test.printMatrix(ar);                      // печать матрицы
-        Matrix matrix2 = new Matrix(ar);      // создание объекта
+        int p = RandomInt(10);
+        int q = RandomInt(10);
+        System.out.println("Размер массива: " + p + " на " + q);
+        double[][] ar = new double[p][q]; // создаем двумерный массив случайного размера
+        System.out.println("Создаем обьект matrix3");
+        Matrix matrix3 = new Matrix(ar);      // создание объекта
+
+        System.out.print("matrix1: ");
+        Matrix.printMatrix(matrix1.getMatrix());                      // печать матрицы
 
     }
 
+    static int RandomInt(int i){
+        return 1+(int)(Math.random() * i);
+    }
 }
