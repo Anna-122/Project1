1d3040243d5a43679bd3506f7f765b2855a915c9
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-10-02 18:14:44 +0300

    abstract class with method

--- src/com/uits/task12/Trumpet.java
+++ src/com/uits/task12/Trumpet.java
@@ -1,4 +1,13 @@
 package com.uits.task12;
 
-public class Trumpet {
+public  abstract class Trumpet implements Instrument {
+    int size ;
+
+     Trumpet( int size ) {
+         this.size = size;
+     }
+
+    public void play() {
+        System.out.println("Играет труба "+ size);
+    }
 }
