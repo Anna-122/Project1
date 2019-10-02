a227f15b040de9de8657b4f2124b4087f7466841
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-10-02 18:14:08 +0300

    abstract class with method

--- src/com/uits/task12/Guitar.java
+++ src/com/uits/task12/Guitar.java
@@ -1,4 +1,15 @@
 package com.uits.task12;
 
-public class Guitar {
+public  abstract class Guitar implements Instrument {
+    int numberOfstrings;
+
+    Guitar( int num ) {
+        numberOfstrings = num;
+    }
+
+    public void play() {
+        System.out.println("Играет гитара "+numberOfstrings);
+    }
 }
+
+
