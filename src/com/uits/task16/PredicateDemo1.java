b70db2307bc4eba9a13fe1a04b003454e493c3b7
SergMicar <Sergey.V.Goncharov@gmail.com>
2019-10-21 15:36:54 +0300

    The "lambda" example is now work perfectly )

--- src/com/uits/task16/PredicateDemo1.java
+++ src/com/uits/task16/PredicateDemo1.java
@@ -1,15 +1,41 @@
- package com.uits.task16;
-import java.util.concurrent.Callable;
-import java.util.function.Predicate;
-public class PredicateDemo1 {
-    public static void main( String[] args ) {
-       () -> {
-            return System.out.println("I love Java!");
-        }
+package com.uits.task16;
+
+import java.util.Arrays;
+import java.util.List;
+
 //(параметры) -> {тело метода}
-       // Callable<Integer> c = () -> 42;
+// Callable<Integer> c = () -> 42;
+interface FunctionPrint {
+    void call();
+}
 
+class PredicateDemo1 {
+    public static void main(String[] args) {
+        String firstWord = "I love";
+        FunctionPrint function = () -> System.out.println(firstWord + " Java!"); //лямбда выражение как метод интерфеса FunctionPrint
+        function.call(); //вызов лямбды
 
+        System.out.println();
+        System.out.println("****** Примеры оптимизации кода с помощью лямбда выражений ******");
+
+        System.out.println("Классический цикл");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7);
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        System.out.println("Тот же цикл через лямбда");
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7);
+        numbers.forEach((Integer value) -> System.out.println(value));
+
+        System.out.println("Все тот же цикл через лямбда и оператор ::");
+        List<Integer> list2 = Arrays.asList(1,2,3,4,5,6,7);
+        numbers.forEach(System.out::println);
+//        list.stream().map((x) -> x*x).forEach(System.out::println);
     }
 }
+
+
+
+
 
