0b289f0d892661491d30e3f3efeea922467b8831
SergMicar <Sergey.V.Goncharov@gmail.com>
2019-11-08 17:21:42 +0200

    price regex is now work!

--- src/kpi/laba4/PriceList.java
+++ src/kpi/laba4/PriceList.java
@@ -1,18 +1,29 @@
+/*
+Есть текст со списками цен. Извлечь из него цены в USD, RUR, EU.
+– пример правильных выражений: 23.78 USD.
+– пример неправильных выражений: 22 UDD, 0.002 USD.
+*/
 package kpi.laba4;
 import java.util.regex.*;
 
 public class PriceList {
-    String regex;
-    String s = "Цены 30.06 USD 50.30 RUS 60.12 CHF 11,23 23.78 USD" ;
-    Pattern p1 = Pattern.compile(regex);
-    Matcher m1 = p1.matcher(s);
-        while (m1.find()) {
-        System.out.println("Список цен: " + m1.group());}
 
-    public PriceList() {
-        regex = "(\\d+\\.)(\\w+{2,3})*";
+    public static void main( String args[] ) {
+        int count = 0;
+        // Строка для сканирования
+        String s = "Цены 30.06 USD 50.30 RUS 60.12 CHF 11,23 23.78 USD";
+        String pattern = "\\d+(.\\d{1,2}) [USDRUSCHF]{2,3}";
+        // Создание Pattern объекта
+        Pattern p = Pattern.compile(pattern);
+
+        // Создание matcher объекта
+        Matcher m = p.matcher(s);
+        while (m.find()) {
+            count ++;
+            System.out.println("Список цен: " + m.group());
+        }
+        if (count==0) {
+            System.out.println("СОВПАДЕНИЙ НЕ НАЙДЕНО");
+        }
     }
-
-
 }
-
