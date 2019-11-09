3611e180e3f193a884764c15bda753822d5f65eb
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-11-09 15:34:01 +0200

    refactor authentification

--- src/com/uits/task13/Authentification.java
+++ src/com/uits/task13/Authentification.java
@@ -3,28 +3,61 @@ package com.uits.task13;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-    class Authentification extends InternetShop{
-        public static boolean check( String u ) {
-            Pattern pattern = Pattern.compile (^[a-z 0-9_]{ 6, 15}$);
+    class Authentification {
 
-//^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$
+        /*   public static void main( String[] args ) {
+               public static boolean check (String u ){
+                   Pattern pattern = Pattern.compile("^[a-zA-Z_]{6,15}$");
+
+
+                   InputString("login");
+                   if (Authentification.check(password)) ;
+
+
+                   System.out.print("Input password: ");
+                   String password = in.nextString();
+                   if (Authentification.check(password)) ;
+                   System.out.printf("Username: %s  Password: %s   \n", username, password);
+                   System.out.println("password валиден");
+
+                   in.close();
+
+
+           public static InputString(String message) {
+                       Scanner in = new Scanner(System.in);
+                       System.out.print(message + ": ");
+                       String str = in.nextString();
+                       return str;
+                   }
+               }
+
+
+           private static void InputString( String login ) {
+           }
+       }*/
+        public static void auth() {
+            Pattern pattern = Pattern.compile("^[a-zA-Z_]{6,15}$");
+            String login = InputString("login");
+            if (login.equals("") ){
+                System.out.print ( "Аутентифецировался покупатель" );
+            } else {
+                    if (login.equals("^[a-zA-Z_]{6,15}$")) {
+                        System.out.print("login менеджера валиден");
+                        String password = InputString("password");
+                    }
+
+            }
         }
 
-
-
-
-        public static void main( String[] args ) {
-
+        public static String InputString( String message ) {
+            String str = null;
             Scanner in = new Scanner(System.in);
-            System.out.print("Input username: ");
-            String username = in.nextLine();
-
-            System.out.print("Input password: ");
-            String password = in.nextLine();
-            if(Authentification .check(password));
-            System.out.printf("Username: %s  Password: %s   \n", username, password);
-            System.out.println("password валиден");
-
+            System.out.print(message + ": ");
+            while (in.hasNext()) {
+                str = in.nextLine();
+            }
             in.close();
+            return str;
+
         }
     }
---
+++ src/com/uits/task13/MainUserInterface.java
@@ -0,0 +1,7 @@
+package com.uits.task13;
+
+public class MainUserInterface {
+    public static void main( String[] args ) {
+        Authentification.auth();
+    }
+}
