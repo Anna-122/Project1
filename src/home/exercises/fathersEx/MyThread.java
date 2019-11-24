ae8502a988044805a778ee9b0ca2f5b97b7656c6
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-11-24 16:40:00 +0200

    extends Thread

--- src/home/exercises/fathersEx/MyThread.java
+++ src/home/exercises/fathersEx/MyThread.java
@@ -1,22 +1,21 @@
 package home.exercises.fathersEx;
 
+
 /**
  * @author anna
  */
 
 
 
-public class MyThread  {
+public class MyThread extends Thread {
    public static void main( String[] args )throws Exception {
         MyThread myThread = new MyThread();
         myThread.start();
        System.out.print(Thread.currentThread().getName());
     }
 
-    public class Method extends Thread{
-        @Override
-
-        public void run() {
+    public class Method {
+               public void run() {
             System.out.print("This is my Thread");
             someMethod();
         }
